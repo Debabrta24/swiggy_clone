64b51a5f683b16a8c5cd96e6e1e36e3062e83b85
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-02-24 10:57:24 +0530

    oth

--- src/Componenet/MenuCard.jsx
+++ src/Componenet/MenuCard.jsx
@@ -1,117 +1,66 @@
-import React from "react";
-import RestInfo from "../Componenet/RestInfo"
-const MenuCard = () => {
-  const [isOpen, setIsOpen] = useState(true);
+import React, { useState } from "react";
+import RestInfo from "./RestInfo";
 
-  if ("categories" in menuItems) {
+const MenuCard = ({ menuIteams }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  // console.log(menuIteams)
+  if (isOpen) {
     return (
+      <>
+        <div className="flex justify-between">
+          <p className="text-3xl font-bold p-4">{menuIteams.title}</p>
+          <button
+            className="text-3xl "
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            {isOpen ? "^" : "v"}
+          </button>
+        </div>
+      </>
+    );
+  }
+  if ("categories" in menuIteams) {
+    return (
+      <>
+        <div className="w-full">
+          <p className="text-3xl font-bold p-4">{menuIteams.title}</p>
+        
+          <div className="">
+            {menuIteams?.categories?.map((items) => {
+              <MenuCard key={items?.title} menuIteams={items}></MenuCard>;
+            })}
+          </div>
+          
+        <div className="h-5 bg-gray-600 ">Hello ji</div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
       <div className="w-full">
-        <p className="text-2xl font-bold">{menuItems.title}</p>
-        <div>
-          {menuItems?.categories?.map((items) => (
-            <MenuCard
-              key={items?.title}
-              menuItems={items}
-              foodselected={foodselected}
-            ></MenuCard>
+        <p className="font-bold text-2xl">{menuIteams.title}</p>
+          <button
+            className="text-3xl "
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            {isOpen ? "^" : "v"}
+          </button>
+        <div className="">
+          {menuIteams?.itemCards?.map((item) => (
+            <RestInfo
+              key={item?.card?.info?.id}
+              restData={item?.card?.info}
+            ></RestInfo>
           ))}
         </div>
       </div>
-    );
-  }
 
-  if (!isOpen) {
-    return (
-      <div className="w-full">
-        <div className="flex justify-between w-full">
-          <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-          <button
-            className="text-5xl font-bold mr-20"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            {isOpen ? "^" : "⌄"}
-          </button>
-        </div>
-        <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
-      </div>
-    );
-  }
-
-  if (foodselected === "veg") {
-    return (
-      <div className="w-full">
-        <div className="flex justify-between w-full">
-          <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-          <button
-            className="text-5xl font-bold mr-20"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            {isOpen ? "^" : "˯"}
-          </button>
-        </div>
-        <div>
-          {menuItems?.itemCards
-            ?.filter((food) => "isVeg" in food?.card?.info)
-            .map((items) => (
-              <RestInfo
-                key={items?.card?.info?.id}
-                restData={items?.card?.info}
-              ></RestInfo>
-            ))}
-        </div>
-        <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
-      </div>
-    );
-  }
-
-  if (foodselected === "nonveg") {
-    return (
-      <div className="w-full">
-        <div className="flex justify-between w-full">
-          <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-          <button
-            className="text-5xl font-bold mr-20"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            {isOpen ? "^" : "˯"}
-          </button>
-        </div>
-        <div>
-          {menuItems?.itemCards
-            ?.filter((food) => !("isVeg" in food?.card?.info))
-            .map((items) => (
-              <RestInfo
-                key={items?.card?.info?.id}
-                restData={items?.card?.info}
-              ></RestInfo>
-            ))}
-        </div>
-        <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="w-full">
-      <div className="flex justify-between w-full">
-        <p className="text-3xl font-bold mb-4">{menuItems.title}</p>
-        <button
-          className="text-5xl font-bold mr-20"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? "^" : "˯"}
-        </button>
-      </div>
-      <div>
-        {menuItems?.itemCards?.map((items) => (
-          <RestInfo
-            key={items?.card?.info?.id}
-            restData={items?.card?.info}
-          ></RestInfo>
-        ))}
-      </div>
-      <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
-    </div>
+    </>
   );
 };
 
--- src/Componenet/RestInfo.jsx
+++ src/Componenet/RestInfo.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const RestInfo = () => {
+const RestInfo = ({ restData }) => {
+  // console.log( restData )
+
   return (
-    <div>RestInfo</div>
-  )
-}
+    <>
+      <div className="flex justify-between w-full ">
+        <div className="w-[70%]">
+          <p>{restData?.defaultPrice?restData?.defaultPrice/100:restData.price/100}</p>
+          <span className="text-green-700" >{restData?.ratings?.aggregatedRating?.rating}</span>
+          <span  >{"("+restData?.ratings?.aggregatedRating?.ratingCountV2+")"}</span>
+          <p>{restData?.description}</p>
+        </div>
+        <div className="w-[20%] relative">
+          <img src={"https://media-assets.swiggy.com/swiggy/image/upload/"+restData?.imageId} alt="" />
+          <button className="absolute bg-white bottom-1 left-20 text-green-600 px-4 py-2 rounded-xl " >ADD </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default RestInfo
+export default RestInfo;
